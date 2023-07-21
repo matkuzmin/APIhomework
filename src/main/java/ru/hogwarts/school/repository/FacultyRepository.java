7aabc9cbe9edf42ea3e3fd9088f696879f67b0ea
matkuzmin <123067217+matkuzmin@users.noreply.github.com>
2023-07-21 22:18:50 +0300

    ДЗ БД готово(исправлено)

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -1,7 +1,8 @@
 package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.Model.Faculty;
-
+@Repository
 public interface FacultyRepository extends JpaRepository<Faculty,Long> {
 }
--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,7 +1,8 @@
 package ru.hogwarts.school.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.hogwarts.school.Model.Student;
-
+@Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
 }
