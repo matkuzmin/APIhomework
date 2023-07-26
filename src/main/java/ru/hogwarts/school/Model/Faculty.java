ae9e3162494995e0d478f9bd95e03b5cb452e0c4
matkuzmin <123067217+matkuzmin@users.noreply.github.com>
2023-07-26 16:32:20 +0300

    ДЗ потоки готово

--- src/main/java/ru/hogwarts/school/Model/Faculty.java
+++ src/main/java/ru/hogwarts/school/Model/Faculty.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class Faculty {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String color;
