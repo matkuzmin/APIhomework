1d2ba20407b4ab6f8805535c6db52cdb696c083c
matkuzmin <123067217+matkuzmin@users.noreply.github.com>
2023-08-14 00:22:36 +0300

    init

--- src/main/java/ru/hogwarts/school/scripts.sql
+++ src/main/java/ru/hogwarts/school/scripts.sql
@@ -1,5 +1,6 @@
-select * from student where age>10 and age<20
-select name  from student
-select * from  student where name like '%r%'
-select * from student where age<student.id
-select * from student order by age
+select * from student where age>10 and age<20;
+select name  from student;
+select * from  student where name like '%r%';
+select * from student where age<student.id;
+select * from student order by age;
+
